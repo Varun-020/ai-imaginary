5721fb5398c15d4e76cb4e94421079be86772fc6
Varun-020 <varunbaba020@gmail.com>
2024-02-17 10:40:34 +0530

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main className="flex min-h-screen items-center justify-between p-24">
+    </main>
+  )
+}
+
+export default Home
